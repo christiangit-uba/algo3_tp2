3c6b04e7d99285c03b143b70076d5774f21a867e
christiangit-uba <you@example.com>
2021-07-15 23:47:01 -0300

    Pruebas entrega 1

---
+++ src/main/test/edu/fiuba/algo3/Entrega1/PruebaPaisAtacaYGanaDefensorTest.java
@@ -0,0 +1,48 @@
+package edu.fiuba.algo3.Entrega1;
+
+import edu.fiuba.algo3.modelo.*;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PruebaPaisAtacaYGanaDefensorTest {
+    @Test
+    public void unPaisAtacaAunPaisLimitrofeEnemigoYGana(){
+        Color rojo = new Color("rojo");
+        Color azul = new Color("azul");
+
+        Pais Argentina = new Pais("Argentina");
+        Pais Brazil = new Pais("Brazil");
+
+        Argentina.agregarLimitrofe(Brazil);
+        Brazil.agregarLimitrofe(Argentina);
+
+        Argentina.agregarColor(rojo);
+        Brazil.agregarColor(azul);
+
+        Argentina.agregarEjercito();
+        Argentina.agregarEjercito();
+        Brazil.agregarEjercito();
+
+        DadoTrucado dadoAtacante = new DadoTrucado();
+        DadoTrucado dadoDefensor = new DadoTrucado();
+
+        ArrayList<Integer> valoresDadoAtacante = new ArrayList<>();
+        ArrayList<Integer> valoresDadoDefensor = new ArrayList<>();
+
+        valoresDadoAtacante.add(6);
+        valoresDadoDefensor.add(2);
+
+        dadoAtacante.setValores(valoresDadoAtacante);
+        dadoDefensor.setValores(valoresDadoDefensor);
+
+        dadoAtacante.comparar(dadoDefensor,Argentina,Brazil);
+
+        Argentina.ocuparPais(Brazil,rojo);
+
+        assertTrue(Argentina.mismoColor(rojo));
+        assertTrue(Brazil.mismoColor(rojo));
+    }
+}
