008016c8e1d30aa2836615314041b27e13c499be
jghm96 <77647023+jghm96@users.noreply.github.com>
2021-07-28 21:07:45 -0300

    arreglo juego  y rondas

--- src/main/java/edu/fiuba/algo3/modelo/Juego.java
+++ src/main/java/edu/fiuba/algo3/modelo/Juego.java
@@ -5,107 +5,88 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Juego {
-    private static ArrayList<ColorJugador> colores;
+    private int cantidadJugadores;
     private Tablero tablero;
     private Tarjetero tarjetero;
     private ArrayList<Jugador> jugadores;
-    private int jugadorEnTurno;
+    private Jugador jugadorEnTurno;
     private boolean conquistoUnPaisAlmenos = false;
     private Ronda ronda;
 
     public Juego(int cantidad) throws FileNotFoundException {
         tarjetero = new Tarjetero();
         tablero = new Tablero(tarjetero);
-        //jugadores = new ArrayList<>();
-        colores = new ArrayList<>();
-       // inicializarColores();
-       // inicializarJugadores(cantidad);
-       // ronda = new Ronda(jugadores);
+        jugadores = new ArrayList<>();
+        ronda = new Ronda(jugadores);
+        cantidadJugadores = cantidad;
+    }
+    public void IniciarJuego(){
+        ronda = new Ronda(jugadores);
+        ronda.iniciarPrimeraRonda(cantidadJugadores);
     }
     public void agregarJugadores(ArrayList<Jugador> jugadores){
         this.jugadores = jugadores;
         ronda = new Ronda(jugadores);
     }
 
-     private void inicializarColores() {
-        //configurar colores validos
-        colores.add(new ColorJugador("blue","0000FF"));
-        colores.add(new ColorJugador("red","FF0000"));
-        colores.add(new ColorJugador("Amarillo","0x0000FF"));
-        colores.add(new ColorJugador("Verde","0x0000FF"));
-        colores.add(new ColorJugador("Rosa","0x0000FF"));
-        colores.add(new ColorJugador("Negro","0x0000FF"));
-    }
-
-
-
-    private void inicializarJugadores(int cantidad) {
-        int i = 0;
-        while (i < cantidad){
-            jugadores.add(new Jugador("jugador"+(i+1),colores.get(i)));
-            i++;
-        }
-    }
-
-
-
     public Tarjetero getTarjetero(){
         return tarjetero;
     }
 
     public void iniciarTurno(){
-        ronda.jugadorEnRonda().reiniciarTope(tablero);
+        jugadorEnTurno = ronda.jugadorEnRonda();
+        jugadorEnTurno.reiniciarTope(tablero);
     }
 
 
 
     public void atacar(String paisAtacante, String paisDefensor, int cantidadTropas, ArrayList<Integer> valoresDadosAtacante, ArrayList<Integer> valoresDadoDefensor) throws Exception {
-        if(ronda.jugadorEnRonda().realizarAtaque(tablero.obtenerPais(paisAtacante),tablero.obtenerPais(paisDefensor) ,cantidadTropas,valoresDadosAtacante,valoresDadoDefensor)){
+        if(jugadorEnTurno.realizarAtaque(tablero.obtenerPais(paisAtacante),tablero.obtenerPais(paisDefensor) ,cantidadTropas,valoresDadosAtacante,valoresDadoDefensor)){
             conquistoUnPaisAlmenos = true;
         }
     }
 
     public boolean activarTarjeta(Tarjeta tarjeta){
-        return ronda.jugadorEnRonda().activarTarjetaPais(tarjeta);
+        return jugadorEnTurno.activarTarjetaPais(tarjeta);
     }
 
     public boolean canjearTarjetas(ArrayList<Tarjeta> tarjetas){
-        if(ronda.jugadorEnRonda().validarCanjes(tarjetas, tarjetero)){
-            ronda.jugadorEnRonda().canjear();
+        if(jugadorEnTurno.validarCanjes(tarjetas, tarjetero)){
+            jugadorEnTurno.canjear();
             return true;
         }
         return false;
     }
 
     public boolean colocarEjercito(Pais pais, int cantidadAColocar){
-        return ronda.jugadorEnRonda().colocarEjercitos(cantidadAColocar, pais);
+        return jugadorEnTurno.colocarEjercitos(cantidadAColocar, pais);
     }
 
     public int getTope(){
-        return ronda.jugadorEnRonda().getTope();
+        return jugadorEnTurno.getTope();
     }
 
     public boolean terminarTurno() {
         if(conquistoUnPaisAlmenos){
-            tarjetero.asignarTarjeta(ronda.jugadorEnRonda());
+            tarjetero.asignarTarjeta(jugadorEnTurno);
         }
         return ronda.pasarTurno();
     }
 
     public boolean moverEjercito(String origen, String destino, int cantidadEjercitos) throws Exception {
-        return ronda.jugadorEnRonda().realizarMovimiento(tablero.obtenerPais(origen),tablero.obtenerPais(destino),cantidadEjercitos);
+        return jugadorEnTurno.realizarMovimiento(tablero.obtenerPais(origen),tablero.obtenerPais(destino),cantidadEjercitos);
     }
 
     public String colorCodigoJugadorEnTurno() {
-        return ronda.jugadorEnRonda().colorCodigo();
+        return jugadorEnTurno.colorCodigo();
     }
 
     public String nombreJugadorEnTurno() {
-        return ronda.jugadorEnRonda().nombre();
+        return jugadorEnTurno.nombre();
     }
 
     public String colorJugadorEnTurno() {
-        return ronda.jugadorEnRonda().color();
+        return jugadorEnTurno.color();
     }
 
 
